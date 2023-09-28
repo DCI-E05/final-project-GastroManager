26c6939b877f7e5c23a67f2b48caf23e35755d8a
Spencer-OBrien <spencerobrien@live.com>
2023-09-28 14:47:26 +0200

    basic API setup changes to urls, views, admin, reqs

--- gastromanager/api/urls.py
+++ gastromanager/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import staff_view, inventory_view, stock_view
+from .views import staff_view, inventory_view, stock_view, stock_name_view
 
 urlpatterns = [
-    path("stock", stock_view, name="stock"),
-    path("staff", staff_view, name="staff"),
-    path("inventory", inventory_view, name="inventory"),
+    path("stock", stock_view.as_view(), name="stock"),
+    path("staff", staff_view.as_view(), name="staff"),
+    path("inventory", inventory_view.as_view(), name="inventory"),
+    path("stockname", stock_name_view.as_view(), name="stockname"),
 ]
--- gastromanager/api/views.py
+++ gastromanager/api/views.py
@@ -1,20 +1,49 @@
-from django.shortcuts import render
-from .models import StockItem, InventoryItem, StaffMember, WorkingHours
+# from django.shortcuts import render
+# from .models import StockItem, InventoryItem, StaffMember, WorkingHours
 
 
-def stock_view(request):
-    stock = StockItem.objects.first()
-    return render(request)
+# def stock_view(request):
+#     stock = StockItem.objects.first()
+#     return render(request)
 
 
-def inventory_view(request):
-    inventory = InventoryItem.objects.first()
-    return render(request)
+# def inventory_view(request):
+#     inventory = InventoryItem.objects.first()
+#     return render(request)
 
 
-def staff_view(request):
-    staff = StaffMember.objects.first()
-    return render(request)
+# def staff_view(request):
+#     staff = StaffMember.objects.first()
+#     return render(request)
 
 
-# Create your views here.
+# # Create your views here.
+
+from rest_framework import generics
+from .models import InventoryItem, StockItem, StaffMember, StockItemName
+from .serializers import (
+    InventoryItemSerializer,
+    StockItemSerializer,
+    StaffMemberSerializer,
+    StockItemNameSerializer,
+)
+
+
+class inventory_view(generics.ListCreateAPIView):
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer
+
+
+class stock_name_view(generics.ListCreateAPIView):
+    queryset = StockItemName.objects.all()
+    serializer_class = StockItemNameSerializer
+
+
+class stock_view(generics.ListCreateAPIView):
+    queryset = StockItem.objects.all()
+    serializer_class = StockItemSerializer
+
+
+class staff_view(generics.ListCreateAPIView):
+    queryset = StaffMember.objects.all()
+    serializer_class = StaffMemberSerializer
