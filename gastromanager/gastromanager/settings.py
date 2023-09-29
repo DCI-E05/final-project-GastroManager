2c4ccd8b326247385b615bb4564c986f4c847290
Connor <connorm@web.de>
2023-09-29 14:16:24 +0200

    edit setting for workflow

--- gastromanager/gastromanager/settings.py
+++ gastromanager/gastromanager/settings.py
@@ -80,16 +80,28 @@ WSGI_APPLICATION = "gastromanager.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "gastromanager",
-        "USER": "postgres",
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "5432",
+if os.environ.get("CI"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "testdb",
+            "USER": "user",
+            "PASSWORD": "password",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "gastromanager",
+            "USER": "postgres",
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+    }
 
 
 # Password validation
