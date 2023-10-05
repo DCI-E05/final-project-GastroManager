d36fda2b33dbae529a475f55f3cb1e8fff75b9f9
Connor <connorm@web.de>
2023-10-05 15:10:43 +0200

    added swagger documentation

--- gastromanager/api/views.py
+++ gastromanager/api/views.py
@@ -1,24 +1,3 @@
-# from django.shortcuts import render
-# from .models import StockItem, InventoryItem, StaffMember, WorkingHours
-
-
-# def stock_view(request):
-#     stock = StockItem.objects.first()
-#     return render(request)
-
-
-# def inventory_view(request):
-#     inventory = InventoryItem.objects.first()
-#     return render(request)
-
-
-# def staff_view(request):
-#     staff = StaffMember.objects.first()
-#     return render(request)
-
-
-# # Create your views here.
-
 from rest_framework import generics
 from .models import InventoryItem, StockItem, StaffMember, StockItemName
 from .serializers import (
