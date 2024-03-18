5ddc0651eba8f7d7dada7d3f935c670b24082e14
Connor <120728378+cmccann92@users.noreply.github.com>
2024-03-18 11:09:20 +0100

    Update settings.py

--- gastromanager/gastromanager/settings.py
+++ gastromanager/gastromanager/settings.py
@@ -162,5 +162,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'web.de'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'connorm@web.de'
-EMAIL_HOST_PASSWORD = 'r@ttenjunge'
